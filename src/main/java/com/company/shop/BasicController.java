f30b156db3ed7fb9541f8eb129af829341165188
pdy1207 <pdyme1207@gmail.com>
2025-02-03 18:30:13 +0900

    🏗️ 메인페이지 테스트용도

---
+++ src/main/java/com/company/shop/BasicController.java
@@ -0,0 +1,21 @@
+package com.company.shop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDateTime;
+
+@Controller
+public class BasicController {
+    @GetMapping("/")
+    String main(){
+        return "index.html";
+    }
+
+    @GetMapping("/date")
+    @ResponseBody
+    String dateTest(){
+        return LocalDateTime.now().toString();
+    }
+}
--- src/main/java/com/company/shop/ShopApplication.java
+++ src/main/java/com/company/shop/ShopApplication.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShopApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(ShopApplication.class, args);
+
 	}
 
 }
