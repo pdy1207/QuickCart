542e11f72235369d83ac72994e57f2e1f1b6820a
pdy1207 <pdyme1207@gmail.com>
2025-02-05 23:27:57 +0900

    🚀 Azure 배포 CICD테스트

--- src/main/java/com/company/shop/Item.java
+++ src/main/java/com/company/shop/Item.java
@@ -1,13 +1,27 @@
 package com.company.shop;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
+@ToString
+@Getter
+@Setter
 public class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto increment
-    public Long id;
-    public String title;
-    public Integer price;
+    private Long id;
+
+    private String title;
+    private Integer price;
+
+    public Item() {}
+
+    public Item(String title, Integer price) {
+        this.title = title;
+        this.price = price;
+    }
 }
 
--- src/main/java/com/company/shop/ItemController.java
+++ src/main/java/com/company/shop/ItemController.java
@@ -1,16 +1,66 @@
 package com.company.shop;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Controller
+@RequiredArgsConstructor
 public class ItemController {
 
-    @GetMapping("/list")
-    String list(Model model){
-        model.addAttribute("name","비싼바지");
+    private final ItemRepository itemRepository;
 
+/*
+    Lombok X
+    @Autowired
+    public ItemController(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+*/
+
+    @GetMapping("/list")
+    String list(Model model) {
+        List<Item> result = itemRepository.findAll();
+        model.addAttribute("items", result);
         return "list.html";
     }
+
+    @GetMapping("/write")
+    String write() {
+        return "write.html";
+    }
+
+    @PostMapping("/add")
+    String addPost(@RequestParam Map<String, String> formDate) {
+
+        String title = formDate.get("title");
+        Integer price = Integer.parseInt(formDate.get("price"));
+
+        Item item = new Item(title,price);
+
+        itemRepository.save(item);
+
+        return "redirect:/list";
+    }
+
+    @GetMapping("/detail/{id}")
+    String addPost(@PathVariable Long id, Model model) {
+        Optional<Item> result = itemRepository.findById(id);
+        if( result.isPresent() ){
+            model.addAttribute("item", result.get());
+        } else {
+            model.addAttribute("errorMessage", "해당 아이템을 찾을 수 없습니다.");
+        }
+
+        return "detail.html";
+    }
+
 }
